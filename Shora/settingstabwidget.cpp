7155506eb3b6dde61f44e2ff0314bf97266cea98
Florian <florianblas82@gmail.com>
2022-11-26 13:31:12 +0100

    Adding the settings tab base structure

--- Shora/settingstabwidget.cpp
+++ Shora/settingstabwidget.cpp
@@ -1,6 +1,33 @@
 #include "settingstabwidget.h"
 
-SettingsTabWidget::SettingsTabWidget(QWidget* parent)
-    : QWidget(parent){
+#include <QSpacerItem>
 
+SettingsTabWidget::SettingsTabWidget(QWidget* parent)
+    : QWidget(parent)
+{
+    m_layout = new QHBoxLayout;
+    m_buttonLayout = new QVBoxLayout;
+
+    m_layout->addLayout(m_buttonLayout);
+
+    m_usbCamSettings = new QPushButton;
+    m_buttonLayout->addWidget(m_usbCamSettings);
+    m_usbCamSettings->setText("USB camera setup");
+
+    m_customVideoSettings = new QPushButton;
+    m_buttonLayout->addWidget(m_customVideoSettings);
+    m_customVideoSettings->setText("Video stream settings");
+
+    m_slideShow = new QPushButton;
+    m_buttonLayout->addWidget(m_slideShow);
+    m_slideShow->setText("Create slide show");
+
+    m_buttonLayout->addStretch(1);
+
+    m_layout->addSpacerItem(new QSpacerItem(1,100));
+
+    m_settingsStack = new QStackedWidget;
+    m_layout->addWidget(m_settingsStack);
+
+    setLayout(m_layout);
 }
--- Shora/settingstabwidget.h
+++ Shora/settingstabwidget.h
@@ -2,11 +2,25 @@
 #define SETTINGSTAB_H
 
 #include <QWidget>
+#include <QStackedWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class SettingsTabWidget : public QWidget
 {
 public:
     SettingsTabWidget(QWidget* parent = nullptr);
+
+private:
+    QHBoxLayout* m_layout;
+    QVBoxLayout* m_buttonLayout;
+
+    QPushButton* m_usbCamSettings;
+    QPushButton* m_customVideoSettings;
+    QPushButton* m_slideShow;
+
+    QStackedWidget* m_settingsStack;
 };
 
 #endif // SETTINGSTAB_H
